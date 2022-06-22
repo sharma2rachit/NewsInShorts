d644ce9cd4e52cf7b2ff10b73b0287e8ac6b2645
sharma2rachit <73378516+sharma2rachit@users.noreply.github.com>
2022-06-23 00:51:15 +0530

    Initial commit

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='NewsInshorts',
+    version='1.0',
+    packages=[''],
+    url='',
+    license='',
+    author='rachit sharma',
+    author_email='rs188162@gmail.com',
+    description=''
+)
